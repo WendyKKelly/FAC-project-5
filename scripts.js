0db662f989c375adcf18ef00e5f68b8990ca70a6
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-07-18 16:53:00 -0700

    prev-btn

--- scripts.js
+++ scripts.js
@@ -20,4 +20,19 @@ nextSlide.addEventListener("click", function () {
     });
 });
 
-const 
+
+const prevSlide = document.querySelector(".btn-prev");
+
+
+prevSlide.addEventListener("click", function () {
+  
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
