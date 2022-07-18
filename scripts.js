4626195a193ac341c17021e26a5bc007eb1e5614
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-07-18 15:39:00 -0700

    overflow comment out

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const slides = document.querySelectorAll(".slide");
 
 slides.forEach((slide, index) => {
-    slide.style.transform = 'translateX(${index * 100}%)';
+    slides.style.transform = 'translateX(${index * 100}%)';
 });
